71ee00020ea9b40dc58d12a21c8ab8fe29efd059
SiongSng <rrt467778@gmail.com>
2022-05-13 21:34:50 +0800

    feat: init flutter

---
+++ android/app/src/main/kotlin/com/example/lipoic/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.lipoic.application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LipoicAPP());
+}
+
+class LipoicAPP extends StatelessWidget {
+  const LipoicAPP({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lipoic',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home page"),
+      ),
+      body: const Center(child: Text('hello world')),
+    );
+  }
+}
