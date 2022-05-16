ac362f7662919593270c5202c8c9df56206d6e5c
xiaomiao-fong <tomylin567@gmail.com>
2022-05-16 22:08:43 +0800

    Login50%

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './pages/Login.dart';
 
 void main() {
   runApp(const LipoicAPP());
@@ -14,7 +15,7 @@ class LipoicAPP extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const LoginPage(),
     );
   }
 }
