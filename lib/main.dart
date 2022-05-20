5a142a2f1bcb7cae754c5e84170990556b639b7a
SiongSng <rrt467778@gmail.com>
2022-05-20 22:37:38 +0800

    fix: wait init config db

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'src/lipoic_app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  ConfigHelper.init();
+  await ConfigHelper.init();
   runApp(const LipoicAPP());
 }
 
